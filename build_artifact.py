9b905736b1e4a292f0ff3f31631540931a0995ff
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2019-11-26 09:17:27 +0100

    Support arm64 build on all Ubuntu versions.

--- build_artifact.py
+++ build_artifact.py
@@ -37,7 +37,7 @@ strCfg_jonchkiHerePath = os.path.join(
     'jonchki'
 )
 # This is the Jonchki version to use.
-strCfg_jonchkiVersion = '0.0.4.1'
+strCfg_jonchkiVersion = '0.0.5.1'
 # Look in this folder for Jonchki archives before downloading them.
 strCfg_jonchkiLocalArchives = os.path.join(
     strCfg_projectFolder,
@@ -70,8 +70,6 @@ strCfg_jonchkiProjectConfiguration = os.path.join(
 # -
 # --------------------------------------------------------------------------
 
-print(tPlatform)
-
 astrCMAKE_COMPILER = None
 astrCMAKE_PLATFORM = None
 astrJONCHKI_SYSTEM = None
@@ -80,62 +78,13 @@ astrEnv = None
 
 if tPlatform['host_distribution_id'] == 'ubuntu':
     if tPlatform['distribution_id'] == 'ubuntu':
-        if tPlatform['cpu_architecture'] == 'arm64':
-            # Build on linux for raspebrry.
-            # Create the folders if they do not exist yet.
-            astrFolders = [
-                strCfg_workingFolder,
-                os.path.join(strCfg_workingFolder, 'packages'),
-            ]
-            for strPath in astrFolders:
-                if os.path.exists(strPath) is not True:
-                    os.makedirs(strPath)
+        # Build on linux for linux.
+        # It is currently not possible to build for another version of the OS.
+        if tPlatform['distribution_version'] != tPlatform['host_distribution_version']:
+            raise Exception('The target Ubuntu version must match the build host.')
 
-#            packagesPath = os.path.join(strCfg_workingFolder, 'packages')
-#            os.chdir(packagesPath)
-#            subProcessPath = os.path.join(strCfg_projectFolder, 'cmake', 'tools')
-#            subProcessCall = '%s/get_dependencies.sh libudev-dev:arm64' % subProcessPath
-#            print(subProcessCall)
-#            subprocess.check_call(subProcessCall, shell=True)
-
-            os.chdir(strCfg_workingFolder)
-            astrCMAKE_COMPILER = [
-                '-DCMAKE_TOOLCHAIN_FILE=%s/cmake/toolchainfiles/toolchain_ubuntu_arm64.cmake' % strCfg_projectFolder
-            ]
-            astrCMAKE_PLATFORM = [
-                '-DJONCHKI_PLATFORM_DIST_ID=ubuntu',
-                '-DJONCHKI_PLATFORM_DIST_VERSION=18.04',
-                '-DJONCHKI_PLATFORM_CPU_ARCH=arm64'
-            ]
-            astrJONCHKI_SYSTEM = [
-                '--distribution-id ubuntu',
-                '--distribution-version 18.04',
-                '--cpu-architecture arm64'
-            ]
-            strMake = 'make'
-
-        else:
-            # Build on linux for linux.
-            # It is currently not possible to build for another version or CPU architecture.
-            if(
-                (tPlatform['distribution_version'] != tPlatform['host_distribution_version']) or
-                (tPlatform['cpu_architecture'] != tPlatform['host_cpu_architecture'])
-            ):
-                raise Exception('The target Ubuntu platform must match the build host.')
-
-#            # Check for all system dependencies.
-#            astrDeb = [
-#                'libudev-dev'
-#            ]
-#            astrInstall = []
-#            for strDeb in astrDeb:
-#                strDpkgStatus = subprocess.check_output("dpkg-query -W -f='${Status}' %s || echo 'unknown'" % strDeb, shell=True)
-#                print('Check for %s = %s' % (strDeb, strDpkgStatus))
-#                if strDpkgStatus != 'install ok installed':
-#                    astrInstall.append(strDeb)
-#            if len(astrInstall) != 0:
-#                subprocess.check_call('sudo apt-get update --assume-yes', shell=True)
-#                subprocess.check_call('sudo apt-get install --assume-yes %s' % ' '.join(astrInstall), shell=True)
+        if tPlatform['cpu_architecture'] == tPlatform['host_cpu_architecture']:
+            # Build for the build host.
 
             astrCMAKE_COMPILER = []
             astrCMAKE_PLATFORM = [
@@ -146,8 +95,31 @@ if tPlatform['host_distribution_id'] == 'ubuntu':
             astrJONCHKI_SYSTEM = []
             strMake = 'make'
 
+        elif tPlatform['cpu_architecture'] == 'arm64':
+            # Build on linux for raspebrry.
+
+            astrCMAKE_COMPILER = [
+                '-DCMAKE_TOOLCHAIN_FILE=%s/cmake/toolchainfiles/toolchain_ubuntu_arm64.cmake' % strCfg_projectFolder
+            ]
+            astrCMAKE_PLATFORM = [
+                '-DJONCHKI_PLATFORM_DIST_ID=%s' % tPlatform['distribution_id'],
+                '-DJONCHKI_PLATFORM_DIST_VERSION=%s' % tPlatform['distribution_version'],
+                '-DJONCHKI_PLATFORM_CPU_ARCH=%s' % tPlatform['cpu_architecture']
+            ]
+
+            astrJONCHKI_SYSTEM = [
+                '--distribution-id %s' % tPlatform['distribution_id'],
+                '--distribution-version %s' % tPlatform['distribution_version'],
+                '--cpu-architecture %s' % tPlatform['cpu_architecture']
+            ]
+            strMake = 'make'
+
+        else:
+            raise Exception('Unknown CPU architecture: "%s"' % tPlatform['cpu_architecture'])
+
     elif tPlatform['distribution_id'] == 'windows':
         # Cross build on linux for windows.
+
         if tPlatform['cpu_architecture'] == 'x86':
             # Build for 32bit windows.
             astrCMAKE_COMPILER = [
@@ -189,7 +161,10 @@ if tPlatform['host_distribution_id'] == 'ubuntu':
         raise Exception('Unknown distribution: "%s"' % tPlatform['distribution_id'])
 
 else:
-    raise Exception('Unknown host distribution: "%s"' % tPlatform['host_distribution_id'])
+    raise Exception(
+        'Unknown host distribution: "%s"' %
+        tPlatform['host_distribution_id']
+    )
 
 # Create the folders if they do not exist yet.
 astrFolders = [
@@ -220,7 +195,7 @@ print(strProjectVersionVcs, strProjectVersionVcsLong)
 
 # ---------------------------------------------------------------------------
 #
-# Get the build requirements for LUA5.1 and the externals.
+# Get the build requirements for LUA5.1.
 #
 strCwd = os.path.join(strCfg_workingFolder, 'lua5.1', 'build_requirements')
 for strMatch in glob.iglob(os.path.join(strCwd, 'lua5.1-lualxi-*.xml')):
@@ -241,7 +216,7 @@ subprocess.check_call(' '.join(astrCmd), shell=True, cwd=strCwd, env=astrEnv)
 subprocess.check_call(strMake, shell=True, cwd=strCwd, env=astrEnv)
 
 astrMatch = glob.glob(os.path.join(strCwd, 'lua5.1-lualxi-*.xml'))
-if len(astrMatch)!=1:
+if len(astrMatch) != 1:
     raise Exception('No match found for "lua5.1-lualxi-*.xml".')
 
 astrCmd = [
@@ -330,6 +305,7 @@ astrCmd.extend(astrJONCHKI_SYSTEM)
 astrCmd.append('--build-dependencies')
 astrCmd.append(astrMatch[0])
 subprocess.check_call(' '.join(astrCmd), shell=True, cwd=strCwd, env=astrEnv)
+
 
 # ---------------------------------------------------------------------------
 #
